6b1a36512d84fa7d06e3e68313b2ae4fbd59ccaf
claudiambeke <mbekeclaudia@gmail.com>
2021-07-16 19:21:39 +0300

    cs50 preprocessor command

--- hello.c
+++ hello.c
@@ -1,5 +1,8 @@
-#include<stdio.h>
+#include <cs50.h>
+#include <stdio.h>
 
-int main(void){
-    printf("Hello Claudia \n");
+int main(void)
+{
+    string name = get_string("What is your name ?");
+    printf("Hello, %s\n", name);
 }
